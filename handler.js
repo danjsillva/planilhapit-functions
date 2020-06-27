4b81a19ece735af83999b4a53bf818abd0d855ea
Daniel Silva <danjsillva@gmail.com>
2020-06-27 11:42:44 -0300

    refactor: alterado nome do parametro label para symbol

--- handler.js
+++ handler.js
@@ -3,10 +3,11 @@
 const axios = require('axios');
 
 module.exports.quotation = async event => {
-  const label = event.queryStringParameters.label
-  const response = (await axios.get(`https://api.hgbrasil.com/finance/stock_price?key=1c0aeaa5&symbol=${label}`)).data;
+  const symbol = event.queryStringParameters.symbol
+  const response = (await axios.get(`https://api.hgbrasil.com/finance/stock_price?key=1c0aeaa5&symbol=${symbol}`)).data;
 
   return {
+    headers: { "Access-Control-Allow-Origin" : "*" },
     statusCode: 200,
     body: JSON.stringify({ ...response.results }, null, 2),
   };
